9fd62f0e55bbecb221d36125c06520671766c8a9
AtsukoTanishita <supernova.abh@gmail.com>
2025-06-11 17:58:30 +0900

    Aboutセクションの追加

--- template-parts/content-about.php
+++ template-parts/content-about.php
@@ -1,6 +1,14 @@
-<section class="about">
+<section class="about-section">
     <div class="container">
-        <h3>About Marnie</h3>
-        <p>お母様のプロフィールをここに載せます。</p>
+        <h2>About Marnie</h2>
+        <div class="about-content">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/marnie.JPG" alt="Marnieのプロフィール写真">
+            <div class="about-text">
+                <p>かれこれ長年の趣味となったかぎ針の編み物。</p>
+                <p>音楽や自然が好きで、季節ごとのお花も楽しみの一つ。</p>
+                <p>InstagramではMarnieのKawaii世界を発信しています。</p>
+                <a href="https://www.instagram.com/marnie.ty/" target="_blank" class="btn">Instagramを見る</a>
+            </div>
+        </div>
     </div>
 </section>
