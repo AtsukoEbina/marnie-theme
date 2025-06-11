373aac21e4d7671da6cf3ceea78f2d3b26cf4660
AtsukoTanishita <supernova.abh@gmail.com>
2025-06-11 17:58:45 +0900

    インスタグラムセクションの追加

--- template-parts/content-instagram.php
+++ template-parts/content-instagram.php
@@ -1,8 +1,31 @@
 <section class="instagram-preview">
     <div class="container">
-        <h3>Latest from Instagram</h3>
-        <div class="instagram-posts">
-            <p>Coming Soon...</p>
+        <h3>Instagramから</h3>
+        <div class="instagram-grid">
+
+            <div class="instagram-item">
+                <blockquote class="instagram-media"
+                    data-instgrm-permalink="https://www.instagram.com/p/CcYtj62hiFE/"
+                    data-instgrm-version="14">
+                </blockquote>
+            </div>
+
+            <div class="instagram-item">
+                <blockquote class="instagram-media"
+                    data-instgrm-permalink="https://www.instagram.com/p/CjtQyUihLp_/"
+                    data-instgrm-version="14">
+                </blockquote>
+            </div>
+
+            <div class="instagram-item">
+                <blockquote class="instagram-media"
+                    data-instgrm-permalink="https://www.instagram.com/p/CfmokBwBWr1/"
+                    data-instgrm-version="14">
+                </blockquote>
+            </div>
+
         </div>
+
+        <script async src="//www.instagram.com/embed.js"></script>
     </div>
 </section>
