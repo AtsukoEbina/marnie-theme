8b5a9865d36f96f56e99b1a0b7f53d8b0e5139b9
AtsukoTanishita <supernova.abh@gmail.com>
2025-06-28 17:39:56 +0900

    表示順番の変更

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 
 <main class="main-content">
     <?php get_template_part('template-parts/content-hero'); ?>
-    <?php get_template_part('template-parts/content-instagram'); ?>
     <?php get_template_part('template-parts/content-about'); ?>
+    <?php get_template_part('template-parts/content-instagram'); ?>
 </main>
 
 <?php get_footer(); ?>
