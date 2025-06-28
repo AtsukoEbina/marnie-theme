913af3ad4b3776b6280f020cb049ab77e149c255
AtsukoTanishita <supernova.abh@gmail.com>
2025-06-28 17:40:38 +0900

    スライドショーの追加

--- template-parts/content-hero.php
+++ template-parts/content-hero.php
@@ -1,6 +1,7 @@
 <section class="hero">
-    <div class="container">
-        <h2>ただ「Kawaii」世界</h2>
+    <div class="hero-background"></div>
+    <div class="hero-content">
+        <h2>Amimono Marnie</h2>
         <p>誰かの心が少しでも癒されますように</p>
     </div>
 </section>
