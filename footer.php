d620821c7c5ddccdec5956e754112e815f694742
AtsukoTanishita <supernova.abh@gmail.com>
2025-06-28 17:38:44 +0900

    ローディング

--- footer.php
+++ footer.php
@@ -1,3 +1,8 @@
+<div id="loading-screen">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/yarn_pink.png" alt="Loading" class="spinner" />
+    <p>Loading...</p>
+</div>
+
 <footer class="site-footer">
     <div class="container">
         <p>&copy; <?php echo date('Y'); ?> Marnie. All rights reserved.</p>
@@ -7,5 +12,3 @@
 </body>
 
 </html>
-
-
