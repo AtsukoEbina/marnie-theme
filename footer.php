2fab2a84843f1b905f1c299feee53487d69fd0c3
AtsukoTanishita <supernova.abh@gmail.com>
2025-06-30 15:02:05 +0900

    ローディングの調整

--- footer.php
+++ footer.php
@@ -1,13 +1,22 @@
-<div id="loading-screen">
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/yarn_pink.png" alt="Loading" class="spinner" />
-    <p>Loading...</p>
-</div>
+<?php if (is_front_page()) : ?>
+    <div id="loading-screen">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/yarn_pink.png" alt="Loading" class="spinner" />
+        <p>Loading...</p>
+    </div>
+<?php endif; ?>
 
 <footer class="site-footer">
     <div class="container">
         <p>&copy; <?php echo date('Y'); ?> Marnie. All rights reserved.</p>
     </div>
 </footer>
+<div id="modal-overlay" class="modal-overlay">
+    <div class="modal-content">
+        <img src="" alt="拡大画像" id="modal-image">
+        <span class="modal-close">&times;</span>
+    </div>
+</div>
+
 <?php wp_footer(); ?>
 </body>
 
